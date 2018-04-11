d3daae5e345a5c32596c5a08c4596d829b3e2b06
Noni <noni.manzano@gmail.com>
2018-04-11 17:35:20 -0500

    solved

--- how_many_feelings/index.js
+++ how_many_feelings/index.js
@@ -24,23 +24,30 @@
 
 function countFeelings(string, array) {
   //good luck
-  let words;
   let count = 0;
 
-  for(let x in array){
-    words = array[x].split('');
-    // console.log(words);
+  for (let x in array) {
+    let words = array[x].split('');
+    let compareWord = [];
 
-    for(let y in string){
-      if (words.indexOf(string[y])) {
-        count++
+    for (let y in string) {
+      let letters = string[y];
+
+      if (words.includes(letters) && !compareWord.includes(letters)) {
+        compareWord.push(letters)
       }
-    
     }
-    console.log(count);
+
+    if (compareWord.length === words.length) {
+      count++
+    }
+
   }
+  return ((count > 1 || count < 1) ? count.toString() + ' feelings.' : count.toString() + ' feeling.')
 }
 
 
 
-countFeelings('yliausoenvjw', ['anger', 'awe', 'joy', 'love', 'grief']) //'3 feelings.'
+// countFeelings('yliausoenvjw', ['anger', 'awe', 'joy', 'love', 'grief']) //'3 feelings.'
+// countFeelings('angerw', ['anger', 'awe', 'joy', 'love', 'grief']) //'2 feelings.'
+countFeelings('xjexdtnvffgrzqivhekzzahrffurmahtvsbwqqgzlyvslgtxgjrxdggppfmmmqqpywdmjnobrvjfhjumjitauthzm', ['anger', 'love', 'grief', 'shame']) //'2 feelings.'
