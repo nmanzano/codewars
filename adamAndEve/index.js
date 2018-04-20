492a619e58a341bb578ddf3a44cc151c2ed1c691
Noni <noni.manzano@gmail.com>
2018-04-19 23:11:33 -0500

    setup question

---
+++ adamAndEve/index.js
@@ -0,0 +1,9 @@
+// According to the creation myths of the Abrahamic religions, Adam and Eva were the first Humans to wander the earth.
+//
+// You have to do God’s job. The creation method must return an array of length 2 containing objects representing Adam and Eva. The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
+// https://www.codewars.com/kata/basic-subclasses-adam-and-eve/train/python
+
+def God():
+    #code
+
+#code
