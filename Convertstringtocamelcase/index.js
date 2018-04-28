a03035d5e3024e43bce9283982dcb0fe7b47c181
Noni <noni.manzano@gmail.com>
2018-04-27 22:53:42 -0500

    setup problem

---
+++ Convertstringtocamelcase/index.js
@@ -0,0 +1,17 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
+//
+// Examples
+// toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+//
+// toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
+
+// link: https://www.codewars.com/kata/convert-string-to-camel-case/train/javascript
+
+function toCamelCase(str){
+
+}
+
+toCamelCase(''), '', "An empty string was provided but not returned")
+toCamelCase("the_stealth_warrior"), "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
+toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value")
+toCamelCase("A-B-C"), "ABC", "toCamelCase('A-B-C') did not return correct value")
