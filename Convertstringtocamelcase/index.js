f91a01c28f696aa0d0010b34fd2ffaa9c9225b5b
Noni <noni.manzano@gmail.com>
2018-04-30 21:30:58 -0500

    WIP

--- Convertstringtocamelcase/index.js
+++ Convertstringtocamelcase/index.js
@@ -9,9 +9,17 @@
 
 function toCamelCase(str){
 
+  if (str.includes('_')) {
+    let num = str.indexOf('_') + 1
+    let final = str.charAt(num).toUpperCase()
+    let work = str.replace(str[4], final)
+    console.log(work);
+  }
+  let cheese = str.replace(/_/g, '');
+  // console.log(cheese)
 }
 
-toCamelCase(''), '', "An empty string was provided but not returned")
-toCamelCase("the_stealth_warrior"), "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
-toCamelCase("The-Stealth-Warrior"), "TheStealthWarrior", "toCamelCase('The-Stealth-Warrior') did not return correct value")
-toCamelCase("A-B-C"), "ABC", "toCamelCase('A-B-C') did not return correct value")
+// toCamelCase('') //, '', "An empty string was provided but not returned")
+toCamelCase("the_stealth_warrior")//, "theStealthWarrior", "toCamelCase('the_stealth_warrior') did not return correct value")
+// toCamelCase("The-Stealth-Warrior")//, "TheStealthWarrior"//, "toCamelCase('The-Stealth-Warrior') did not return correct value")
+// toCamelCase("A-B-C")//, "ABC", "toCamelCase('A-B-C') did not return correct value")
